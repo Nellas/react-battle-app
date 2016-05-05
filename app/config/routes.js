8997fc797f85f3c6362aeec7f27d2933570c625b
Luke Engelhardt <luke@securitymetrics.com>
2016-05-05 11:14:34 -0600

    saving work

---
+++ app/config/routes.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var Main = require('../components/main');
+var Home = require('../components/home');
+
